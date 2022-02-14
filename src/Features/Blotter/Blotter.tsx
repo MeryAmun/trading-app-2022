2329a4673e7aeb8a8ee34ea09edb9af4c40ef059
MeryAmun <chebesiawah@gmail.com>
2022-02-14 14:09:39 -0800

    blotter sketch

---
+++ src/Features/Blotter/Blotter.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Typography }from '@mui/material'
+
+export const Blotter: React.FC = (): JSX.Element => {
+  return (
+    <div>
+        <Typography variant='h3' component='div'>
+  Blotter
+        </Typography>
+    </div>
+  )
+}
---
+++ src/Features/index.ts
@@ -0,0 +1,1 @@
+export { Blotter } from './Blotter/Blotter'
