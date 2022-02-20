dcf2299fe36fee5877fda70af7642b41a85765e9
MeryAmun <chebesiawah@gmail.com>
2022-02-20 15:52:37 +0100

    ticket schema

--- src/Features/Tradeticket/Tradeticket.tsx
+++ src/Features/Tradeticket/Tradeticket.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import {Typography }from '@mui/material'
+import React from "react";
+import { Divider, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import CircularProgress from "@mui/material/CircularProgress";
+import CloudOffIcon from "@mui/icons-material/CloudOff";
+import { useTicketApis } from "./hooks/useTicketApis";
+import { TicketSections } from "./components/TicketSections";
 
-export const Tradeticket: React.FC = (): JSX.Element => {
+const TicketStyledWrapper = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  height: "100vh",
+  marginTop: 20,
+  padding: theme.spacing(0, 1),
+}));
+
+export const TradeTicket: React.FC = (): JSX.Element => {
+  const { loading, error, ticketId, ccyList, cptyList, bondList } =
+    useTicketApis();
   return (
-    <div>
-        <Typography variant='h3' component='div'>
-  Trade ticket
-        </Typography>
-    </div>
-  )
-}
+    <TicketStyledWrapper>
+      <Typography component="div" variant="subtitle1">
+        Trade Details
+      </Typography>
+      <Divider sx={{ m: 2 }} />
+      {loading && <CircularProgress color="secondary" />}
+      {error && <CloudOffIcon color="secondary" sx={{ fontSize: 50 }} />}
+      {!loading && !error && (
+        <TicketSections
+          ticketId={ticketId?.newId}
+          ccyList={ccyList}
+          cptyList={cptyList}
+          bondList={bondList}
+        />
+      )}
+    </TicketStyledWrapper>
+  );
+};
